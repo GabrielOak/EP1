6dd6f20e2b7e108572bd7a94ef2cff74d89d5309
Gabriel Carvalho <gabriel315df@gmail.com>
2017-09-16 15:06:18 -0300

    Criação da classe Glinder

---
+++ inc/glinder.hpp
@@ -0,0 +1,11 @@
+#include "grade.hpp"
+#ifndef GLINDER_HPP
+#define GLINDER_HPP
+
+class Glinder : public Grade{
+public:
+  Glinder();
+  ~Glinder();
+};
+
+#endif
---
+++ src/glinder.cpp
@@ -0,0 +1,11 @@
+#include "glinder.hpp"
+
+Glinder::Glinder(){
+
+  setForma(10,10);
+  setForma(10,11);
+  setForma(10,12);
+  setForma(9,12);
+  setForma(8,11);
+}
+Glinder::~Glinder(){}
