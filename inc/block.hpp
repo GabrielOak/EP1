0e083a1c80b8d03b1188f3b41595b0193c444a7e
Gabriel Carvalho <gabriel315df@gmail.com>
2017-09-16 15:02:37 -0300

    Criação da classe Block

---
+++ inc/block.hpp
@@ -0,0 +1,11 @@
+#include "grade.hpp"
+#ifndef BLOCK_HPP
+#define BLOCK_HPP
+
+class Block : public Grade{
+public:
+  Block();
+  ~Block();
+};
+
+#endif
---
+++ src/block.cpp
@@ -0,0 +1,11 @@
+#include "block.hpp"
+
+Block::Block(){
+
+  setForma(10,10);
+  setForma(10,11);
+  setForma(11,10);
+  setForma(11,11);
+
+}
+Block::~Block(){}
