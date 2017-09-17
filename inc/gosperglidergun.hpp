1c3f9f15e9c357adf57a3b322958812f6e957b81
Gabriel Carvalho <gabriel315df@gmail.com>
2017-09-17 12:22:58 -0300

    Criação da classe GosperGliderGun

---
+++ inc/gosperglidergun.hpp
@@ -0,0 +1,10 @@
+#include "grade.hpp"
+#ifndef GOSPERGLIDERGUN_HPP
+#define GOSPERGLIDERGUN_HPP
+
+class GosperGliderGun : public Grade{
+public:
+  GosperGliderGun();
+  ~GosperGliderGun();
+};
+#endif
---
+++ src/gosperglidergun.cpp
@@ -0,0 +1,42 @@
+#include "gosperglidergun.hpp"
+GosperGliderGun::GosperGliderGun(){
+
+  setForma(6,1);
+  setForma(6,2);
+  setForma(6,11);
+  setForma(6,15);
+  setForma(6,17);
+  setForma(6,18);
+  setForma(6,23);
+  setForma(6,25);
+  setForma(7,17);
+  setForma(7,11);
+  setForma(7,25);
+  setForma(8,12);
+  setForma(8,16);
+  setForma(9,13);
+  setForma(9,14);
+  setForma(1,25);
+  setForma(2,23);
+  setForma(2,25);
+  setForma(3,13);
+  setForma(3,14);
+  setForma(3,21);
+  setForma(3,22);
+  setForma(3,35);
+  setForma(3,36);
+  setForma(4,12);
+  setForma(4,16);
+  setForma(4,21);
+  setForma(4,22);
+  setForma(4,35);
+  setForma(4,36);
+  setForma(5,1);
+  setForma(5,2);
+  setForma(5,11);
+  setForma(5,17);
+  setForma(5,21);
+  setForma(5,22);
+
+}
+GosperGliderGun::~GosperGliderGun(){}
